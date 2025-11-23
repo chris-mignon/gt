7b38dbc2e15ffed014746b622eb33b79f0488d24
christopher mignon <dspmosfet@gmail.com>
2025-11-22 20:21:40 -0400

    g

--- grader/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$DB_HOST = "localhost";
-$DB_NAME = "grading_system1";
-$DB_USER = "root";
-$DB_PASS = "";
--- grader/includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-function require_login() {
-    if (!isset($_SESSION['user_id'])) {
-        header("Location: login.php");
-        exit;
-    }
-}
